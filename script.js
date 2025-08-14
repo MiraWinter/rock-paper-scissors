58cc938e41a2dccbf6c4907bce23f44b0dd8584f
Mira Winter <185725517+MiraWinter@users.noreply.github.com>
2025-08-14 03:02:52 +0000

    Add function to play a round of the game

--- script.js
+++ script.js
@@ -18,9 +18,31 @@ function getHumanChoice() {
     return prompt("What is your choice?")
 }
 
-function playRound() {
-
+function playRound(humanChoice, computerChoice) {
+    if (humanChoice === computerChoice){
+        console.log("It's a tie!")
+    }
+    else if (humanChoice == "rock" && computerChoice == "scissors"
+            || humanChoice == "paper" && computerChoice == "rock"
+            || humanChoice == "scissors" && computerChoice == "paper") {
+        console.log("Human wins!");
+        humanScore++;
+    }
+    else if (computerChoice == "rock" && humanChoice == "scissors"
+            || computerChoice == "paper" && humanChoice == "rock"
+            || computerChoice == "scissors" && humanChoice == "paper") {
+        console.log("Computer wins!");
+        computerScore++;
+    }
+    else {
+        console.log("Invalid input");
+    }
 }
+
+const humanSelection = getHumanChoice().toLowerCase();
+const computerSelection = getComputerChoice();
+
+playRound(humanSelection, computerSelection);
 
 function playGame() {
 
